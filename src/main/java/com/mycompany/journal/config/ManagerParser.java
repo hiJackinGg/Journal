4ebd72adb29804fda8eaa4dea99f858746579b64
Denys Kuznetsov <kuzdenval@gmail.com>
2016-02-01 21:51:04 +0200

    Manager parser added.

---
+++ src/main/java/com/mycompany/journal/config/ManagerParser.java
@@ -0,0 +1,36 @@
+package com.mycompany.journal.config;
+
+import com.mycompany.journal.db.model.Manager;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Retrives all subordinates of specified manager (including delegates).
+ */
+public class ManagerParser {
+
+    public static Set<Manager> findSubordinates(Manager manager){
+        boolean isUser = true;
+        Set<Manager> set = new TreeSet<>();
+
+        findSubordinates(manager, isUser, set);
+
+        return set;
+    }
+
+    private static void findSubordinates(Manager manager, boolean isUser, Set<Manager> set) {
+
+        if(!isUser)
+            set.add(manager);
+
+        for(Manager m : manager.getDelegatedFrom()){
+            findSubordinates(m, false, set);
+        }
+
+        for(Manager m : manager.getSubordinates()){
+            findSubordinates(m, false, set);
+        }
+
+    }
+}
